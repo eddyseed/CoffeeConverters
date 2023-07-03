eb7f07a84ba7e203361b76ec4823447ced39cc30
Rishabh Jain <rishabhjms99@gmail.com>
2023-07-03 20:47:43 +0530

    Add bgcolor to Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,16 +7,17 @@ export default function Navbar() {
 
     return (
         <div id='navbar'>
-            <nav className='m'>
+            <nav className='grid grid-flow-col'>
+                <div className='flex items-center justify-center'>
+                    <Link className='audio-fs tracking-wider leading-9 text-3xl select-none cursor-pointer text-white' to={'/'}>COFFEE <br/>CONVERTERS</Link>
+                </div>
                 <div className='flex space-x-10 items-center h-full justify-end'>
                     <Link to={'/'} className={`${location.pathname === '/' ? "active" : ""}`}>HOME</Link>
                     <Link to={'/merchandise'} className={`${location.pathname === '/merchandise' ? "active" : ""}`}>MERCHANDISE</Link>
                     <Link to={'/community'} className={`${location.pathname === '/community' ? "active" : ""}`}>COMMUNITY</Link>
                     <Link to={'/about'} className={`${location.pathname === '/about' ? "active" : ""}`}>ABOUT</Link>
                 </div>
-                <div>
-
-                </div>
+                <div></div>
             </nav>
         </div>
     )
